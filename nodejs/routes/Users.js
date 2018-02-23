036c4284e7788865d9e8cc83d80340775f7918b2
omarwalid55 <36336200+omarwalid55@users.noreply.github.com>
2018-02-24 00:05:49 +0200

    updated index and users

--- nodejs/routes/Users.js
+++ nodejs/routes/Users.js
@@ -3,7 +3,8 @@ var userSchema = new mongoose.userSchema({
   username: (type: String , unique: true),
   password : {type : String),
   firstname : String,
-  lastname : String
+  lastname : String,
+  email : String
 });
 //Creating a collection (data base Table)
 var User = mongoose.model('myuser', userSchema);
--- nodejs/routes/index.js
+++ nodejs/routes/index.js
@@ -45,12 +45,16 @@ Router.post('/register', function(req , res){
   var password = req.body.password;
   var firstname = req.body.firstname;
   var lastname = req.body.lastname;
+  var email = req.body.email;
+
 
   var newuser = new User();
   newuser.username = username;
   newuser.password = password;
   newuser.firstname = firstname;
   newuser.lastname = lastname;
+  newuser.email = email;
+
   newuser.save(function(err , savedUser){
     if(err){
       console.log(err);
