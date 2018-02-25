adbce31b6debebdc9c9d12c210daba9c73ac3850
Mohamed Magdy <mohamed.moussaaly@student.guc.edu.eg>
2018-02-25 15:01:14 +0200

    adding mousa-self-card into self-card

--- vue-project/src/components/Dashboard/Views/Overview.vue
+++ vue-project/src/components/Dashboard/Views/Overview.vue
@@ -1,22 +1,32 @@
 <template>
   <div>
+  <h1>About Us</h1>
+  <p>Bi Sixteen team is a Gucian team that include third year students
+  faculty of Business Informatics who work hardly to make a good website like
+  e-bay website.</p>
+  <br>
     <!--Self-Cards-->
-      <div class="row">
-        <div class="col-sm-6 col-md-4 col-lg-3" v-for="person in persons">
-          <div class="thumbnail">
-            <b-img v-bind:src="person.image" center fluid-grow alt="image"></b-img>
+    <div class="row">
+      <div class="col-sm-6 col-md-4 col-lg-3" v-for="person in persons">
+        <div class="thumbnail">
+           <b-img v-bind:src="person.image" center fluid-grow alt="image"></b-img>
             <div class="caption">
-              <h3>{{person.name}}</h3>
-              <p>{{person.about}}</p>
-            </div>
+            <h3>{{person.name}}</h3>
+            <p>{{person.about}}</p>
           </div>
         </div>
-      </div>
+
+    </div>
+
+    </div>
+
 
   </div>
 </template>
 <script>
+
   export default {
+
     /**
      * Each tutorial member's data is saved here
      */
@@ -29,9 +39,9 @@
             image: './static/img/people/example.jpg'
           },
           {
-            name: 'Name',
-            about: 'About Me',
-            image: './static/img/people/example.jpg'
+            name: 'Mohamed Magdy',
+            about: 'A third year BI student who seeks for finishing the project.',
+            image: './static/img/mousa.jpg'
           }
         ]
       }
