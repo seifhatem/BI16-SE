5f3f68ae84e6cb5c44ad679b9db90a61de42505b
aly hussein <aly_hussein1234@yahoo.com>
2018-02-27 16:40:02 +0200

    update

--- vue-project/src/components/Dashboard/Views/Register.vue
+++ vue-project/src/components/Dashboard/Views/Register.vue
@@ -6,30 +6,34 @@
          style = "width:150px;height:150px;">
     <h3> Profile information: </h3>
       <label for="uname"><b>Username: </b></label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-   <input type="text" v-model="input" placeholder="Enter Username" name="uname" required>
+   <input type="text" v-model="uname" placeholder="Enter Username"  required>
    <br></br>
    <label for="psw"><b>Password: </b></label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-   <input type="password" v-model="input" placeholder="Enter Password" name="psw" required>
+   <input type="password" v-model="psw" placeholder="Enter Password" required>
     <br></br>
   <span style="font-weight: bold; color : #C0392B  ">Just a few more boxes to fill</span>
         <h3> Personal information: </h3>
    <label for="fname"><b>Full name: </b></label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-   <input type="text" v-model ="input" placeholder="Full name here" name="fname" required>
+   <input type="text" v-model ="fname" placeholder="Full name here"  required>
    <br></br>
    <label for="DOB"><b>Date of birth: </b></label> &nbsp;
-   <input type="Date" v-model="input" placeholder="Date of birth" name="DOB" required>
+   <input type="Date" v-model="DOB" placeholder="Date of birth"  required>
    <br></br>
    <label for="addr"><b>Address: : </b></label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-   <input type="text" v-model= "input" placeholder="Enter Address" name="addr" required>
+   <input type="text" v-model= "addr" placeholder="Enter Address" required>
    <br></br>
    <label for="email"><b>Email-address:</b></label> &nbsp;
-   <input type="text" v-model="input" placeholder="Enter Email" name="email" required>
+   <input type="text" v-model="email" placeholder="Enter Email" required>
 <br></br>
 <br></br>
-  <button type="button" v-model="clicked" class="submitbtn" color: >submit</button>&nbsp; <button type="button" vmodel="clicked" class="cancelbtn">Cancel</button>
+  <button type="button" v-model="btn1" class="submitbtn" color: >submit</button>&nbsp; <button type="button" vmodel="btn2" class="cancelbtn">Cancel</button>
   <br></br>
 
      </div>
       </div>
         </div>
 </template>
+
+<script>
+
+</script>
